e5f1eb1de3e1af2d27177312253bebfdada5c93c
dave <68038998+dzieger@users.noreply.github.com>
2024-12-31 13:14:25 -0500

    DB credentials added via ENV variables in IDE. Custom schema generation enabled

---
+++ collabrium/src/main/java/com/dzieger/collabrium/CollabriumApplication.java
@@ -0,0 +1,13 @@
+package com.dzieger.collabrium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollabriumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollabriumApplication.class, args);
+	}
+
+}
