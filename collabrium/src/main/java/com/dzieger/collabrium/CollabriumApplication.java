d65bf032baccc0674532e3214fab3cc4eadea5fb
dave <68038998+dzieger@users.noreply.github.com>
2024-12-31 15:30:14 -0500

    dev: Spring Security configured successfully. User and Admin accounts created in UserDetailsManager

--- collabrium/src/main/java/com/dzieger/collabrium/CollabriumApplication.java
+++ collabrium/src/main/java/com/dzieger/collabrium/CollabriumApplication.java
@@ -2,8 +2,10 @@ package com.dzieger.collabrium;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.dzieger")
 public class CollabriumApplication {
 
 	public static void main(String[] args) {
